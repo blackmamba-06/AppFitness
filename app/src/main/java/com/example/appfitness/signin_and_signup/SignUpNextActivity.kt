a4ab3d23b571e45cd7d48c6d4d2ca48de661f18b
blackmamba-06 <112746182+blackmamba-06@users.noreply.github.com>
2023-07-30 15:37:57 -0500

    commit change name for correct in layout of SignUpNextActivity

--- app/src/main/java/com/example/appfitness/signin_and_signup/SignUpNextActivity.kt
+++ app/src/main/java/com/example/appfitness/signin_and_signup/SignUpNextActivity.kt
@@ -2,15 +2,15 @@ package com.example.appfitness.signin_and_signup
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.appfitness.databinding.ActivitySignInNextBinding
+import com.example.appfitness.databinding.ActivitySignUpNextBinding
 import com.example.appfitness.objects.ButtonsNavigation
 import com.example.appfitness.objects.ColorNotificationBarWhite
 
 class SignUpNextActivity : AppCompatActivity() {
-    private lateinit var binding: ActivitySignInNextBinding
+    private lateinit var binding: ActivitySignUpNextBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivitySignInNextBinding.inflate(layoutInflater)
+        binding = ActivitySignUpNextBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
